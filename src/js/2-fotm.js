ad1f9c98d5629e76b9d5c0526ecf4eedf21e8eb7
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2024-10-17 10:52:18 +0300

    hw-09-wow

---
+++ src/js/2-fotm.js
@@ -0,0 +1,49 @@
+const form = document.querySelector('.feedback-form');
+const formData = { email: '', message: '' };
+
+function saveToLocalStorage() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function loadFromLocalStorage() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  if (name === 'email') {
+    formData.email = value.trim();
+  } else if (name === 'message') {
+    formData.message = value.trim();
+  }
+
+  saveToLocalStorage();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+  form.elements.email.value = '';
+  form.elements.message.value = '';
+});
+
+loadFromLocalStorage();
